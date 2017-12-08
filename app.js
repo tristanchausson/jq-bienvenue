ba72e2897089a4fd09210adc766e36654c413fde
tristanchausson <tristanchausson@me.com>
2017-12-08 11:06:10 +0100

    resolve exercise

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log('have fun !');
+function formulaire(){
+
+	var obj = {
+	prenom : $("#first_name").val(),
+	nom : $("#last_name").val(),
+	ville : $("#city").val(),
+};
+		console.log(obj.prenom, obj.nom, obj.ville);
+
+$('#username').html(obj.prenom + ' ' + obj.nom + ' de ' + obj.ville);
+}
+$("button").click(formulaire);
+
+
+
+
+	//	$("#first_name").text(e.which);
+	//	$("#last_name").text(e.which);
+	//	$("#city").text(e.which);
+
+
+
+	//		$('#first_name').text(firstname);
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+
+});
